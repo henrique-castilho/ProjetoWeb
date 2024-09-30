ecee50a7e9edd9953f8b415b0f8e8b4db9bda7b9
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-09-30 14:03:15 -0300

    Verifica o campo do E-mail

--- src/app/esqueci/esqueci.component.ts
+++ src/app/esqueci/esqueci.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-esqueci',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './esqueci.component.html',
   styleUrl: './esqueci.component.css'
 })
 export class EsqueciComponent {
+  public mensagem: string = "";
+  public obj: Cliente = new Cliente();
 
+  public reenviarIntrucoes(){
+    if(this.obj.email === "") {
+      this.mensagem = "O campo e-mail é obrigatorio.";
+    } else {
+      this.mensagem ="as intruções foram enviadas para o e-mail: " + this.obj.email;
+    }
+  }
 }
