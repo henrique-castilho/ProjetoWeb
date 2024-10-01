d4a516b03b898c0794508b93711e43b56bd05e25
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-09-30 23:16:26 -0300

    Cria funcionamento da tela cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  public mensagem: string = ""
+  public user: Cliente = new Cliente()
 
+  public gravar(){
+    if (this.user.nome != "" && this.user.email != "" && this.user.documento != "" && this.user.telefone != "" &&
+      this.user.logradouro != "" && this.user.cep != "" && this.user.cidade != "" && this.user.senha != ""
+    ) {
+      localStorage.setItem("cliente", JSON.stringify(this.user))
+      this.mensagem = "Cadastro atualizado/feito com sucesso!"
+    } else {
+      this.mensagem = "Preencha todos os campos para realizar o cadastro ou atualiza"
+    }
+  }
+  
+  public carregar(){
+    let json = localStorage.getItem("cadastro")
+    if(json == null){
+      window.location.href="./login"
+    } else {
+      this.user = JSON.parse(json)
+    }
+  }
+
+  constructor(){
+    this.carregar()
+  }
 }
+
