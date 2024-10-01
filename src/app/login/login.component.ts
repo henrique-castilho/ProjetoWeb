d44ee741d8d007fde0b87e02ee75b54f369b8768
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-09-30 23:25:13 -0300

    Cria funcionamento/função de fazer login e criar cadastro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  public mensagem: string = "Preencha o email e senha para fazer login:"
+  public user: Cliente = new Cliente()
 
+  public fazerLogin(){
+    if(this.user.email == "usuario@gmail.com" && this.user.senha == "1234") {
+      localStorage.setItem("cadastro", JSON.stringify(this.user))
+      window.location.href="./cadastro"
+    } else {
+      this.mensagem = "Email ou senha ivalidos !!"
+      localStorage.removeItem("cadastro")
+    }
+  }
+
+  public criarCadastro(){
+    localStorage.setItem("cadastro", JSON.stringify(this.user))
+    window.location.href="./cadastro"
+  }
 }
