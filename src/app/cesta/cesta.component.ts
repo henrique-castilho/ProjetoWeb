1e909dadd46f73a59f78597a1a5cc0a357a40f9a
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-10-04 00:53:41 -0300

    Cria funções de excluir itens e esvaziar a cesta

--- src/app/cesta/cesta.component.ts
+++ src/app/cesta/cesta.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Cesta } from '../model/cesta';
+import { Item } from '../model/item';
 
 @Component({
   selector: 'app-cesta',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cesta.component.html',
   styleUrl: './cesta.component.css'
 })
 export class CestaComponent {
+  public mensagem: string = "";
+  public cesta: Cesta = new Cesta();
 
+  constructor(){
+    let json = localStorage.getItem("cesta");
+    if(json!=null) {
+      this.cesta = JSON.parse(json);
+    } else {
+      this.mensagem = "Carrinho sem itens, adicione novos itens!";
+    }
+  }
+
+  public excluiItens(obj:Item) {
+    this.cesta.itens = this.cesta.itens.filter(item => item != obj);
+    this.cesta.total = 0;
+    for(let i=0; i<this.cesta.itens.length; i++){
+      this.cesta.total = this.cesta.itens[i].valor+this.cesta.total;
+    }
+    console.log(this.cesta);
+    localStorage.setItem("cesta", JSON.stringify(this.cesta));
+  }
+
+  public esvaziaCarrinho(){
+    localStorage.removeItem("cesta");
+    this.cesta = new Cesta();
+    this.mensagem = "Carrinho sem itens, adicione novos itens!";
+  }
 }
