7a94e4c8b87eb4163d312cce831e23880f5bc475
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-12 23:05:27 -0300

    Cria projeto back-end usando SpringBoot e configura com o banco de dados

---
+++ ProjetoWeb_back/src/main/java/com/fatec/ProjetoWeb_back/ProjetoWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.ProjetoWeb_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebBackApplication.class, args);
+	}
+
+}
---
+++ ProjetoWeb_back/src/test/java/com/fatec/ProjetoWeb_back/ProjetoWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.ProjetoWeb_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
