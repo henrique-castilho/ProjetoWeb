1e70d3608eebf5a6210bf133f5a584a0a4b53a37
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-15 15:39:43 -0300

    Cria o service de cliente para Integração do cadastro de cliente entre o front-end e o back-end

---
+++ ProjetoWeb_front/src/app/services/cliente.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cliente } from '../model/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor(private http: HttpClient) { }
+
+  gravar(obj: Cliente) : Observable<Object> {
+    return this.http.post("http://localhost:8080/api/cliente", obj);
+  }
+
+  alterar(obj: Cliente) : Observable<Object> {
+    return this.http.put("http://localhost:8080/api/cliente", obj);
+  }
+
+  carregar(codigo: number): Observable<any> {
+    return this.http.get("http://localhost:8080/api/cliente/"+ codigo)
+  }
+
+  remover(codigo: number): Observable<Object> {
+    return this.http.delete("http://localhost:8080/api/cliente/" + codigo)
+  }
+
+  listar(): Observable<any> {
+    return this.http.get("http://localhost:8080/api/clientes")
+  }
+}
