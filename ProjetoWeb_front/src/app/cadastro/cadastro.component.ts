e564abf3f286da58892ee42a92269ae1aabd6a2e
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-15 15:54:43 -0300

    Implementação do método de gravação de cliente com o back-end para salvar dados

--- ProjetoWeb_front/src/app/cadastro/cadastro.component.ts
+++ ProjetoWeb_front/src/app/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Cliente } from '../model/cliente';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ClienteService } from '../services/cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,36 +11,31 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
+
 export class CadastroComponent {
   public mensagem: string = ""
   public user: Cliente = new Cliente()
 
-  public gravar(){
-    if (this.user.nome != "" && this.user.email != "" && this.user.documento != "" && this.user.telefone != "" && this.user.logradouro != "" 
-        && this.user.cep != "" && this.user.cidade != "" && this.user.uf != "" && this.user.senha != "" && this.user.confirmar != "") 
-      {
-     if(this.user.senha === this.user.confirmar) {
-      localStorage.setItem("cadastro", JSON.stringify(this.user));
-      this.mensagem = "Cadastro atualizado/feito com sucesso!";
-     } else {
-      this.mensagem = "Senhas diferentes foram inseridas"
-     }
-    } else {
-      this.mensagem = "Preencha todos os campos para realizar o cadastro ou atualizá-lo"
-    }
+  constructor(private service: ClienteService){}
+
+  public gravar() {
+    this.service.gravar(this.user).subscribe({
+      next: (data: any) => {
+        if (data.mensagem.includes('Erro')) {
+          this.mensagem = data.mensagem;
+        } else {
+          this.mensagem = data.mensagem;
+          this.limpar();
+        }
+      },
+      error: (err) => {
+        console.error(err);
+        this.mensagem = "Ocorreu um erro, tente mais tarde!";
+      }
+    });
   }
   
-  public carregar(){
-    let json = localStorage.getItem("cadastro")
-    if(json == null){
-      window.location.href="./login"
-    } else {
-      this.user = JSON.parse(json)
-    }
-  }
-
-  constructor(){
-    this.carregar()
+  public limpar(){
+    this.user = new Cliente();
   }
 }
-
