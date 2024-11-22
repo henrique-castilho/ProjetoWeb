0528f40f22fefa1ac58d563839a49344c021aebb
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-21 23:25:37 -0300

    Cria repository de cesta e item

--- ProjetoWeb_back/src/main/java/com/fatec/ProjetoWeb_back/Repository/CestaRepository.java
+++ ProjetoWeb_back/src/main/java/com/fatec/ProjetoWeb_back/Repository/CestaRepository.java
@@ -1,9 +1,10 @@
 package com.fatec.ProjetoWeb_back.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.fatec.ProjetoWeb_back.Entity.Cesta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CestaRepository extends JpaRepository<Cesta, Integer>{
 
 }
--- ProjetoWeb_back/src/main/java/com/fatec/ProjetoWeb_back/Repository/ItemRepository.java
+++ ProjetoWeb_back/src/main/java/com/fatec/ProjetoWeb_back/Repository/ItemRepository.java
@@ -1,9 +1,10 @@
 package com.fatec.ProjetoWeb_back.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.fatec.ProjetoWeb_back.Entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
     
 }
