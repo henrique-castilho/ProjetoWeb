9f25b1262e7ec0d7e8834ed9a241d3c0989b461e
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-28 02:13:18 -0300

    Implementa funcionalidade de login com mensagem de boas-vindas

--- ProjetoWeb_front/src/app/login/login.component.ts
+++ ProjetoWeb_front/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Cliente } from '../model/cliente';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ClienteService } from '../services/cliente.service';
 
 @Component({
   selector: 'app-login',
@@ -14,14 +15,28 @@ export class LoginComponent {
   public mensagem: string = "Preencha o email e senha para fazer login:"
   public user: Cliente = new Cliente()
 
-  public fazerLogin(){
-    if(this.user.email == "usuario@gmail.com" && this.user.senha == "1234") {
-      localStorage.setItem("cadastro", JSON.stringify(this.user))
-      window.location.href="./cadastro"
-    } else {
-      this.mensagem = "Email ou senha inválidos!"
-      localStorage.removeItem("cadastro")
-    }
+  constructor(private service: ClienteService){}
+
+  public fazerLogin() {
+    this.service.fazerLogin(this.user).subscribe({
+      next: (data) => {
+        if (data.mensagem) {
+          this.mensagem = data.mensagem;
+        } else {
+          this.user = data;
+          if (this.user.codigo) {
+            localStorage.setItem("cliente", JSON.stringify(this.user));
+            localStorage.setItem("loginMessage", `Bem-vindo, ${this.user.nome}!`);
+            window.location.href = "./vitrine";
+          } else {
+            this.mensagem = "Email ou senha inválidos!";
+          }
+        }
+      },
+      error: (msg) => {
+        this.mensagem = "Ocorreu um erro, tente novamente!";
+      }
+    });
   }
 
   public criarCadastro(){
