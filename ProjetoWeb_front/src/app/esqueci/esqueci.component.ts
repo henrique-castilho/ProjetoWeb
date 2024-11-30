f8d1234daa57aceaf9e364014766b582c4cd29c1
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-30 16:35:29 -0300

    Implementar funcionalidade de reenviar instruções para redefinição de senha

--- ProjetoWeb_front/src/app/esqueci/esqueci.component.ts
+++ ProjetoWeb_front/src/app/esqueci/esqueci.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Cliente } from '../model/cliente';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ClienteService } from '../services/cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-esqueci',
@@ -14,11 +16,31 @@ export class EsqueciComponent {
   public mensagem: string = "";
   public user: Cliente = new Cliente();
 
+  constructor(private service: ClienteService, private router: Router){}
+
   public reenviarIntrucoes(){
-    if(this.user.email == "") {
-      this.mensagem = "O campo email é obrigatorio.";
+    if (this.user.email === "") {
+      this.mensagem = "Preencha o campo e-mail.";
     } else {
-      this.mensagem ="As intruções foram enviadas para o email: " + this.user.email;
+      this.service.esqueciSenha(this.user).subscribe({
+        next: (response: any) => {
+          if (response.token) {
+            this.router.navigate(['/redefine-senha'], { 
+              queryParams: { email: this.user.email, token: response.token } 
+            });
+          } else {
+            this.mensagem = response.mensagem || "Erro desconhecido.";
+          }
+        },
+        error: (err) => {
+          if (err.status === 404) {
+            this.mensagem = err.error.mensagem || "E-mail não encontrado, verifique!!";
+          } else {
+            this.mensagem = "Erro ao processar a solicitação.";
+          }
+          console.error(err);
+        }
+      });
     }
   }
 }
