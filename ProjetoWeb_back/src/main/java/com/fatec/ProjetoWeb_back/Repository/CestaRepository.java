43819577c5dd8c585981e81bd089994796883797
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-12-02 03:10:40 -0300

    adiciona método para obter o maior código de cesta no repositório

--- ProjetoWeb_back/src/main/java/com/fatec/ProjetoWeb_back/Repository/CestaRepository.java
+++ ProjetoWeb_back/src/main/java/com/fatec/ProjetoWeb_back/Repository/CestaRepository.java
@@ -2,9 +2,11 @@ package com.fatec.ProjetoWeb_back.Repository;
 
 import com.fatec.ProjetoWeb_back.Entity.Cesta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CestaRepository extends JpaRepository<Cesta, Integer>{
-
+public interface CestaRepository extends JpaRepository<Cesta, Integer> {
+    @Query("SELECT COALESCE(MAX(c.codigo), 0) FROM Cesta c")
+    int codMaximo();
 }
